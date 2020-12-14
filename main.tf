fa2b739ca96b2214e4fd2186a1cee35ac4187e2f
esu <esu@tiatechnology.com>
2020-12-14 08:58:02 +0200

    Added .gitignore main.tf files

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+    region = "eu-central-1"
+    version = "~> 3.20.0"
+}
+
+resource "aws_instance" "example" {
+    ami                     = "ami-0cdae9530d06806d4"
+    instance_type           = "t2.micro"
+    vpc_security_group_ids  = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    tags = {
+        Name = "terraform-example"
+    }    
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
